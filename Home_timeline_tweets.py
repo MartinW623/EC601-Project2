d88ef728e66741af76f139b650c56cdc14201147
MartinW623 <90434799+MartinW623@users.noreply.github.com>
2021-11-10 11:03:17 -0500

    Update Home_timeline_tweets.py

--- Home_timeline_tweets.py
+++ Home_timeline_tweets.py
@@ -1,12 +1,17 @@
-#This funtion is used to download your home timeline tweets and print each one of their texts to the console.
-
 import tweepy
 
+#Set up Twitter API authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+#Download and print out Twitters from the author's home timeline
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
+
+#Create a new txt file to store the results
+f = open (r'Home_timeline_tweets_results.txt','w')
+print (public_tweets,file = f)
+f.close()
